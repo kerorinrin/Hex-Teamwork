5789341002a32f1df2b9204b63ff1c79e6cde2c9
kerorinrin <128007877+kerorinrin@users.noreply.github.com>
2023-05-27 23:04:15 +0800

    0527

---
+++ wk4.js
@@ -0,0 +1,34 @@
+// 資料串接
+const TableBody = document.querySelector('.table-group-divider');
+const A2 = document.querySelector('#a2')
+
+let schoolData=[];
+
+function getData(){
+    axios.get('https://soa.tainan.gov.tw/Api/Service/Get/b1054400-86a4-40fa-aea0-fb39fce361e2')
+    .then(function(response){
+        console.log(response.data);
+        schoolData=response.data.data;
+
+        renderData();
+    })
+}
+
+getData();
+
+// 作品渲染至畫面
+function renderData(){ 
+    let dataContent = '';   
+    schoolData.forEach((item,index) => {
+        dataContent += `<tr><th scope="row">${index+1}</th><td>${item.school}</td><td>${item.num}</td></tr>`
+    });
+    
+    A2.innerHTML=`⇒　臺南市共有${schoolData.length}個國小，大甲國小有${getNumOfStudents('大甲')}位學生`
+    TableBody.innerHTML=dataContent;
+};
+
+// 取得指定學校的學生人數
+function getNumOfStudents(schoolName) {
+    const school = schoolData.find(item => item.school.includes(schoolName));
+    return school ? school.num : 0;
+  }
